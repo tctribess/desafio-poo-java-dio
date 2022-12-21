fbea642bc619b93f316dadc6630f2c99f2df2195
tctribess <tctribess@gmail.com>
2022-12-21 09:17:17 -0300

    Inclusão das Classes Mentoria e Curso

---
+++ src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,26 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Curso apresentando conceitos sobre a linguagem Java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso1.setTitulo("Curso Java POO");
+        curso1.setDescricao("Curso apresentando conceitos sobre programação orientada a objetos em java");
+        curso1.setCargaHoraria(4);
+
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("Mentoria de Java");
+        mentoria.setDescricao("Descrição mentoria Java");
+        mentoria.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+        System.out.println(mentoria);
+    }
+}
